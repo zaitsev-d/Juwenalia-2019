b553e8ac3610cbbdcba073e6e859cdde44c6b6ee
Dmitry Zaitsev <dmitry.zaitsev.1993@gmail.com>
2020-02-07 15:50:20 +0100

    Main Project

---
+++ public/js/switch.js
@@ -0,0 +1,8 @@
+﻿var size;
+function incfont() {
+    var t = document.getElementById('fontsize').value;
+    var x = document.getElementById('text');
+    x.style.fontSize = t + "px";
+    size = t;
+    document.getElementById("size").innerHTML = size;
+}
